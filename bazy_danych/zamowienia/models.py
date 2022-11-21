c3e10755b4c5bfb418802b5e571328ffaa7572bc
Michal-Suhak <81828005+Michal-Suhak@users.noreply.github.com>
2022-11-21 12:06:12 +0100

    Update models.py

--- bazy_danych/zamowienia/models.py
+++ bazy_danych/zamowienia/models.py
@@ -7,7 +7,7 @@ class Zamowienia(models.Model):
     data_zamowienia = models.DateField(auto_created=True)
 
 
-class Sczegoly_zamowienia(models.Model):
+class Szczegoly_zamowienia(models.Model):
     ilosc = models.IntegerField()
     id_zamowienia = models.ForeignKey(Zamowienia, on_delete=models.PROTECT)
     id_produktu = models.ForeignKey(Produkty, on_delete=models.PROTECT)
@@ -15,6 +15,6 @@ class Sczegoly_zamowienia(models.Model):
 
 class Faktury(models.Model):
     id_uzytkownika = models.ForeignKey(Uzytkownicy, on_delete=models.PROTECT)
-    id_zamowienia = models.ForeignKey(Sczegoly_zamowienia, on_delete=models.PROTECT)
+    id_zamowienia = models.ForeignKey(Szczegoly_zamowienia, on_delete=models.PROTECT)
     kwota = models.DecimalField(decimal_places=2, max_digits=10)
     data_wystawienia = models.DateField(auto_created=True)
