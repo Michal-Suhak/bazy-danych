74a93be091ab1df2de01d1ffd7ee49cc61b2d5a2
michal-suhak <michal.suhak@gmail.com>
2022-12-05 20:00:46 +0100

    new users models and user manager

---
+++ bazy_danych/apps/uzytkownicy/managers.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.models import BaseUserManager
+
+class UserManager(BaseUserManager):
+    def _create_user(self, email, password, imie, nazwisko, **extra_fields):
+        """
+            Creates and saves a User with a given eail, password and extra fields
+        """
+        if not email:
+            raise ValueError("Nie podano emaila")
+        email = self.normalize_email(email)
+        user = self.model(
+            email = email,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+    def create_user(self, email, password, imie, nazwisko, **extra_fields):
+        """
+            Create and save a User with the given email and password
+
+            :type email: str
+            :type password: str
+            :type extra_fields: dict
+        """
+        extra_fields.setdefault('admin', False)
+        return self._create_user(email, password, extra_fields)
+
+    def create_superuser(self, email, password, imie, nazwisko, **extra_fields):
+        """
+            Create and save a Super User with the given email and password
+
+            :type email: str
+            :type password: str
+            :type extra_fields: dict
+        """
+        extra_fields.setdefault('admin', True)
+        extra_fields.setdefault('staff', True)
+        return self._create_user(email, password, imie, nazwisko, **extra_fields)
--- bazy_danych/apps/uzytkownicy/models.py
+++ bazy_danych/apps/uzytkownicy/models.py
@@ -1,12 +1,56 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from .managers import UserManager
+
+
+class Uzytkownicy(AbstractBaseUser):
+    email = models.EmailField(max_length=100, unique=True)
+    imie = models.CharField(max_length=50)
+    nazwisko = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    admin = models.BooleanField()
+    staff = models.BooleanField()
+
+    REQUIRED_FIELDS = ['imie', 'nazwisko']
+    USERNAME_FIELD = 'email' 
+
+    objects = UserManager()
+
+    def get_full_name(self):
+        return "{} {}".format(self.imie, self.nazwisko)
+
+    def __str__(self):
+        return self.imie
+
+    def has_perm(self, perm, obj=None):
+        """
+            Does the user have a specific permission?
+        """
+        return True
+
+    def has_module_perms(self, app_label):
+        """
+            Does the user have permissions to view the apps (the app_label)?
+        """
+        return True
+
+    @property
+    def is_admin(self):
+        return self.admin
+
+    @property
+    def is_staff(self):
+        return self.staff
 
 
 class Kontakty(models.Model):
+    id_uzytkownika  = models.OneToOneField(Uzytkownicy, on_delete=models.CASCADE, null=True)
     telefon = models.CharField(max_length=12, null=True, blank=True)
     email = models.CharField(max_length=50, blank=True, null=True)
 
 
 class Adresy(models.Model):
+    id_uzytkownika = models.OneToOneField(Uzytkownicy, on_delete=models.CASCADE, null=True)
     miejscowosc = models.CharField(max_length=70)
     powiat = models.CharField(max_length=50)
     wojewodztwo = models.CharField(max_length=50)
@@ -14,10 +58,3 @@ class Adresy(models.Model):
     ulica = models.CharField(max_length=70, null=True, blank=True)
     numer_domu = models.IntegerField()
     numer_lokalu = models.IntegerField(null=True, blank=True)
-
-
-class Uzytkownicy(models.Model):
-    imie = models.CharField(max_length=50)
-    nazwisko = models.CharField(max_length=50)
-    id_kontaktu  = models.ForeignKey(Kontakty, on_delete=models.CASCADE)
-    id_adresu = models.ForeignKey(Adresy, on_delete=models.CASCADE)
