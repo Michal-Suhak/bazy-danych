bef5192a7b4e6c25335f9901a4e1e073229d7d60
huber_laptop <hubertkawalko@gmail.com>
2022-12-12 08:48:11 +0100

    extra decorator

--- bazy_danych/apps/produkty/decorators.py
+++ bazy_danych/apps/produkty/decorators.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.mixins import AccessMixin
 
 
@@ -8,3 +10,20 @@ class StaffRequiredMixin(AccessMixin):
         if not (request.user.is_authenticated and request.user.is_staff):
             return self.handle_no_permission()
         return super().dispatch(request, *args, **kwargs)
+
+
+def staff_required(
+    function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url=None
+):
+    """
+    Decorator for views that checks that the user is logged in and staff, redirecting
+    to the log-in page if necessary.
+    """
+    actual_decorator = user_passes_test(
+        lambda u: u.is_authenticated and u.is_staff,
+        login_url=login_url,
+        redirect_field_name=redirect_field_name,
+    )
+    if function:
+        return actual_decorator(function)
+    return actual_decorator
